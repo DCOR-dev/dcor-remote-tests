533dc4366219e82a060b61a87ba1fdb1b0264a14
Paul Müller <dev@craban.de>
2021-01-27 21:52:39 +0100

    update tests (setting dataset state to active does not work if there are not resources)

--- tests/test_schemas_basic.py
+++ tests/test_schemas_basic.py
@@ -1,7 +1,13 @@
+import pathlib
+
+import pytest
+
 from dcoraid.dbmodel.model_api import CKANAPI
-from dcoraid.upload import create_dataset
+from dcoraid import upload
 
 from helper import SERVER, get_api_key, make_dataset_dict
+
+data_path = pathlib.Path(__file__).parent / "data"
 
 
 def test_create_dataset():
@@ -9,21 +15,54 @@ def test_create_dataset():
     # create some metadata
     dataset_dict = make_dataset_dict()
     # post dataset creation request
-    data = create_dataset(dataset_dict=dataset_dict,
-                          server=SERVER,
-                          api_key=get_api_key(),
-                          create_circle=True,
-                          activate=True)
+    data = upload.create_dataset(dataset_dict=dataset_dict,
+                                 server=SERVER,
+                                 api_key=get_api_key(),
+                                 create_circle=True,
+                                 activate=False)
     # simple test
     assert "authors" in data
     assert data["authors"] == dataset_dict["authors"]
-    assert data["state"] == "active"
+    # A dataset is considered to be a draft when it does not contain resources
+    assert data["state"] == "draft"
     # make sure it really worked
     api = CKANAPI(server=SERVER, api_key=get_api_key())
     data2 = api.get("package_show", id=data["id"])
     assert "authors" in data2
     assert data2["authors"] == dataset_dict["authors"]
-    assert data2["state"] == "active"
+    assert data2["state"] == "draft"
+
+
+def test_create_dataset_fail_activate_without_resource():
+    # create some metadata
+    dataset_dict = make_dataset_dict()
+    # post dataset creation request
+    with pytest.raises(ValueError):
+        upload.create_dataset(dataset_dict=dataset_dict,
+                              server=SERVER,
+                              api_key=get_api_key(),
+                              create_circle=True,
+                              activate=True)
+
+
+def test_create_dataset_with_resource():
+    # create some metadata
+    dataset_dict = make_dataset_dict()
+    # post dataset creation request
+    data = upload.create_dataset(dataset_dict=dataset_dict,
+                                 server=SERVER,
+                                 api_key=get_api_key(),
+                                 create_circle=True)
+
+    upload.add_resource(dataset_id=data["id"],
+                        path=data_path / "calibration_beads_47.rtdc",
+                        server=SERVER,
+                        api_key=get_api_key(),
+                        )
+
+    upload.activate_dataset(dataset_id=data["id"],
+                            server=SERVER,
+                            api_key=get_api_key())
 
 
 if __name__ == "__main__":
