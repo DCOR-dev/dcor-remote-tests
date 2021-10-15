94d76514842cd96a9b3a590e39e85c0e9f01300d
Paul Müller <dev@craban.de>
2021-10-15 09:44:11 +0200

    tests: update to work with new dcoraid versions

--- tests/test_schemas_basic.py
+++ tests/test_schemas_basic.py
@@ -3,23 +3,23 @@ import pathlib
 import pytest
 
 import dcoraid
-from dcoraid import upload
+from dcoraid.api import dataset
 
 from helper import get_api, make_dataset_dict
 
 data_path = pathlib.Path(__file__).parent / "data"
 
 
-def test_create_dataset():
+def test_dataset_create():
     """All logged-in users should be able to create circles and datasets"""
     # create some metadata
     dataset_dict = make_dataset_dict()
     # post dataset creation request
     api = get_api()
-    data = upload.create_dataset(dataset_dict=dataset_dict,
-                                 api=api,
-                                 create_circle=True,
-                                 activate=False)
+    data = dataset.dataset_create(dataset_dict=dataset_dict,
+                                  api=api,
+                                  create_circle=True,
+                                  activate=False)
     # simple test
     assert "authors" in data
     assert data["authors"] == dataset_dict["authors"]
@@ -32,73 +32,73 @@ def test_create_dataset():
     assert data2["state"] == "draft"
 
 
-def test_create_dataset_public():
+def test_dataset_create_public():
     """All logged-in users should be able to create circles and datasets"""
     # create some metadata
     dataset_dict = make_dataset_dict()
     dataset_dict["private"] = False
     # post dataset creation request
     api = get_api()
-    data = upload.create_dataset(dataset_dict=dataset_dict,
-                                 api=api,
-                                 create_circle=True)
-    upload.add_resource(dataset_id=data["id"],
-                        path=data_path / "calibration_beads_47.rtdc",
-                        api=get_api(),
-                        )
+    data = dataset.dataset_create(dataset_dict=dataset_dict,
+                                  api=api,
+                                  create_circle=True)
+    dataset.resource_add(dataset_id=data["id"],
+                         path=data_path / "calibration_beads_47.rtdc",
+                         api=get_api(),
+                         )
     data2 = api.get("package_show", id=data["id"])
     assert not data2["private"]
-    upload.activate_dataset(dataset_id=data["id"],
-                            api=get_api())
+    dataset.dataset_activate(dataset_id=data["id"],
+                             api=get_api())
 
 
-def test_create_dataset_fail_activate_without_resource():
+def test_dataset_create_fail_activate_without_resource():
     # create some metadata
     dataset_dict = make_dataset_dict()
     # post dataset creation request
     with pytest.raises(dcoraid.api.APIConflictError):
-        upload.create_dataset(dataset_dict=dataset_dict,
-                              api=get_api(),
-                              create_circle=True,
-                              activate=True)
+        dataset.dataset_create(dataset_dict=dataset_dict,
+                               api=get_api(),
+                               create_circle=True,
+                               activate=True)
 
 
-def test_create_dataset_with_resource():
+def test_dataset_create_with_resource():
     # create some metadata
     dataset_dict = make_dataset_dict()
     # post dataset creation request
-    data = upload.create_dataset(dataset_dict=dataset_dict,
-                                 api=get_api(),
-                                 create_circle=True)
+    data = dataset.dataset_create(dataset_dict=dataset_dict,
+                                  api=get_api(),
+                                  create_circle=True)
 
-    upload.add_resource(dataset_id=data["id"],
-                        path=data_path / "calibration_beads_47.rtdc",
-                        api=get_api(),
-                        )
+    dataset.resource_add(dataset_id=data["id"],
+                         path=data_path / "calibration_beads_47.rtdc",
+                         api=get_api(),
+                         )
 
-    upload.activate_dataset(dataset_id=data["id"],
-                            api=get_api())
+    dataset.dataset_activate(dataset_id=data["id"],
+                             api=get_api())
 
 
 def test_delete_dataset_forbidden():
     # create some metadata
     dataset_dict = make_dataset_dict()
     # post dataset creation request
-    data = upload.create_dataset(dataset_dict=dataset_dict,
-                                 api=get_api(),
-                                 create_circle=True)
+    data = dataset.dataset_create(dataset_dict=dataset_dict,
+                                  api=get_api(),
+                                  create_circle=True)
 
-    upload.add_resource(dataset_id=data["id"],
-                        path=data_path / "calibration_beads_47.rtdc",
-                        api=get_api(),
-                        )
+    dataset.resource_add(dataset_id=data["id"],
+                         path=data_path / "calibration_beads_47.rtdc",
+                         api=get_api(),
+                         )
 
-    upload.activate_dataset(dataset_id=data["id"],
-                            api=get_api())
+    dataset.dataset_activate(dataset_id=data["id"],
+                             api=get_api())
 
     with pytest.raises(dcoraid.api.APIAuthorizationError):
-        upload.remove_draft(dataset_id=data["id"],
-                            api=get_api())
+        dataset.dataset_draft_remove(dataset_id=data["id"],
+                                     api=get_api())
 
 
 if __name__ == "__main__":
