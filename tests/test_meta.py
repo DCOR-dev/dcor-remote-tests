7270b01efd353d76ec89fa894eaa81bda0dc5db5
Paul Müller <dev@craban.de>
2023-09-20 10:14:39 +0200

    add DCOR meta tests

---
+++ tests/test_meta.py
@@ -0,0 +1,17 @@
+from dclab.external.packaging import parse as parse_version
+
+from helper import get_api
+
+
+def test_ckan_version():
+    api = get_api()
+    assert api.ckan_version_object >= parse_version("2.10.1")
+
+
+def test_extensions():
+    api = get_api()
+    extensions = api.get("status_show")["extensions"]
+    for key in ["activity", "stats", "text_view", "image_view", "dcor_depot",
+                "dcor_schemas", "dc_serve", "dc_view", "dc_log_view",
+                "dcor_theme"]:
+        assert key in extensions, f"extension '{key}' not loaded!"
