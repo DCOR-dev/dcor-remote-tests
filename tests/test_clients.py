d64836de498bb138a9733aec278538c84b245c8d
Paul Müller <dev@craban.de>
2024-11-23 00:51:29 +0100

    update version checks

--- tests/test_clients.py
+++ tests/test_clients.py
@@ -20,6 +20,8 @@ def test_current_client_version():
     "0.6.9",
     "0.7.1",
     "0.7.6",  # no version branding in dclab.RTDCWriter
+    "0.13.0",
+    "0.14.1",
     ])
 def test_forbidden_client_versions(dcoraid_version):
     """Old clients should not work"""
@@ -27,3 +29,13 @@ def test_forbidden_client_versions(dcoraid_version):
     api.headers["user-agent"] = f"DCOR-Aid/{dcoraid_version}"
     with pytest.raises(errors.APIOutdatedError, match="outdated"):
         api.get("site_read")
+
+
+@pytest.mark.parametrize("dcoraid_version", [
+    "0.15.5",
+    ])
+def test_allowed_client_versions(dcoraid_version):
+    """Old clients should not work"""
+    api = get_api()
+    api.headers["user-agent"] = f"DCOR-Aid/{dcoraid_version}"
+    api.get("site_read")
